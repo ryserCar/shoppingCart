6fdeec7485e2a4228933d862bd057505a683db83
cryser <carlosryser@gmail.com>
2018-08-19 19:15:52 -0300

    Initial commit

---
+++ src/main/java/com/example/trainning/TrainningApplication.java
@@ -0,0 +1,12 @@
+package com.example.trainning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainningApplication.class, args);
+	}
+}
