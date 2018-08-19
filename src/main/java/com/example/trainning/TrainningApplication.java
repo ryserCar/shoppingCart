0766bc755e32c252d80d721f2a7fb2191b1851f1
cryser <carlosryser@gmail.com>
2018-08-19 19:26:35 -0300

    Update gitignore file and indent code

--- src/main/java/com/example/trainning/TrainningApplication.java
+++ src/main/java/com/example/trainning/TrainningApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrainningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrainningApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrainningApplication.class, args);
+    }
 }
